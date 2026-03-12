8947f806448cc5ba494b241c9ad75263cb64c67d
KhalloufiHamza <hamza.khalloufi@usmba.ac.ma>
2026-03-13 00:49:09 +0100

    3) Reflected XSS fix & 4) Vulnerable dependency fix

--- src/main/java/com/example/devsecopsdemo/DemoController.java
+++ src/main/java/com/example/devsecopsdemo/DemoController.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -30,8 +31,8 @@ public class DemoController {
         );
     }
 
-    @GetMapping(value = "/echo", produces = "text/html")
+    @GetMapping(value = "/echo", produces = "text/plain")
     public String echo(@RequestParam String msg) {
-        return "<html><body>" + msg + "</body></html>"; // Intentionally vulnerable to reflected XSS
+        return HtmlUtils.htmlEscape(msg);
     }
 }
